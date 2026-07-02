5b94187f300aa56ab3eb506fb2da0dfebb5875f1
tanayagarwal2006 <write.tanay@gmail.com>
2026-07-02 12:07:47 +0530

    Sending true yaw angle for now, to compensate for lack of magnetometer model

--- simulator/packet_builder.py
+++ simulator/packet_builder.py
@@ -1,13 +1,15 @@
 import struct
 
-def build_euler_packet(euler_rad,body_rates):
+def build_euler_packet(euler_rad,body_rates, true_yaw_angle):
     return struct.pack(
-        "<6f",
+        "<7f",
         euler_rad[0],
         euler_rad[1],
         euler_rad[2],
 
         body_rates[0],
         body_rates[1],
-        body_rates[2]
+        body_rates[2],
+
+        true_yaw_angle
     )
