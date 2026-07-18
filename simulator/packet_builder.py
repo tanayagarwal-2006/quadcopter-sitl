822cf799b16bbc29de41becf6bd867d22b4e0053
tanayagarwal2006 <write.tanay@gmail.com>
2026-07-18 15:48:01 +0530

    Fix: Nomenclature

--- simulator/packet_builder.py
+++ simulator/packet_builder.py
@@ -1,15 +1,15 @@
 import struct
 
-def build_euler_packet(euler_rad,body_rates,vel,true_yaw_angle):
+def build_euler_packet(gyro,accel,vel,true_yaw_angle):
     return struct.pack(
         "<10f",
-        euler_rad[0],
-        euler_rad[1],
-        euler_rad[2],
+        gyro[0],
+        gyro[1],
+        gyro[2],
 
-        body_rates[0],
-        body_rates[1],
-        body_rates[2],
+        accel[0],
+        accel[1],
+        accel[2],
 
         vel[0],
         vel[1],
